10011408a361b475756da6c69fe88d5362b94c02
Arturs Luters <artursluters@gmail.com>
2026-02-26 22:24:35 +0100

    update: made so fuctions have dockstring exampls as asked in the task

--- data_collections.py
+++ data_collections.py
@@ -1,30 +1,30 @@
 # A daļa saraksti
 print("-----Saraksti-----/")
 saraksts = [1, 2, 3, 4, 5]
-print(saraksts)
+print(saraksts) # Sakuma sarakts
 saraksts.append(11)
 saraksts.append(12)
 saraksts.append(13)
-saraksts.append(14)
-print(saraksts)
-saraksts.pop()
-print(saraksts)
-total = 0
-count = 0
-para_saraksts = []
-for i in saraksts:
-    total += i
-    count += 1
-    if i % 2 == 0:
-        para_saraksts.append(i)
-if count > 0:
-    average = total / count
-    print(f"Vidējā vērtība: {average} Summa: {total}")
-    print(f"Pāra skaitļi: {para_saraksts}")
-    print(f"Pirmie 3 skaitļi: {saraksts[:3]} pēdējie 2 skaitļi: {saraksts[-2:]} katrs otrais skaitlis: {saraksts[1:9:2]}")
+saraksts.append(14) # Pievienojam 4 skaitļus
+print(saraksts)   # Saraksts pēc pievienošanas
+saraksts.pop()  # Noņemam pēdējo skaitli
+print(saraksts) # Saraksts pēc noņemšanas
+total = 0  #Summa
+count = 0  #Daudzums
+para_saraksts = [] # Pāra skaitļu saraksts
+for i in saraksts: # Izejam cauri sarakstam pa vienam elementam
+    total += i # Suma 
+    count += 1 # Skaitītājs
+    if i % 2 == 0: # Ja skaitlis ir pāra, pievienojam to pāra sarakstam
+        para_saraksts.append(i) # Pāra skaitļu pievienošana sarakstam
+if count > 0: # Aprēķinām vidējo vērtību, ja skaitītājs nav nulle
+    average = total / count # Vidējā vērtība
+    print(f"Vidējā vērtība: {average} Summa: {total}") # Izvada vidējo vērtību un summu
+    print(f"Pāra skaitļi: {para_saraksts}") # Izvada pāra skaitļus
+    print(f"Pirmie 3 skaitļi: {saraksts[:3]} pēdējie 2 skaitļi: {saraksts[-2:]} katrs otrais skaitlis: {saraksts[1:9:2]}") # Izvada pirmos 3, pēdējos 2 un katru otro skaitli
 
 # B daļa vārdnīcas
-print("\n-----Vārdnīca-----/")
+print("\n-----Vārdnīca-----")
 vardnica = {"Anna": 85, "Jānis": 72, "Līga": 95, "Pēteris": 68}
 vardnica["Marta"] = 90
 for name, grade in vardnica.items():
@@ -33,7 +33,7 @@ for name, grade in vardnica.items():
     if grade == max(vardnica.values()):
         print(f"{name} ir labākais students ar {grade} punktiem")
 # C daļa kombinācija
-print ("n/-----Studenti ar atzīmi >= 80-----/")
+print ("\n-----Studenti ar atzīmi >= 80-----")
 student_grades = [{"name": "Anna", "grade": 85}, {"name": "Jānis", "grade": 72}, {"name": "Līga", "grade": 95}, {"name": "Pēteris", "grade": 68}, {"name": "Marta", "grade": 90}]
 top_students = [t for t in student_grades if t["grade"] >= 80]
 for i, student in enumerate(top_students, start=1):
